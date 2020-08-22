5c8d9d8d03652794b3a4ba7a116dd1f96a6f90b6
Volkov1516 <from5jforever@gmail.com>
2020-08-22 17:44:25 +0300

    22.08.20

--- src/components/articles/comments/Comments.jsx
+++ src/components/articles/comments/Comments.jsx
@@ -1,28 +1,31 @@
-import React from  'react'
+import React, {useState} from  'react'
 import style from './Comments.module.css'
 
 const Comments = () => {
 
-    let [comment, setComment] = React.useState( [
+    let [comments, setComment] = useState( [
         {id: 1, text: "Hello!", date: "Friday, 1 July 2020"},
         {id: 2, text: "Awesome article!", date: "Saturday, 2 July 2020"},
         {id: 3, text: "Just amazing!", date: "Wednesday, 9 July 2020"}
     ])
 
+    {/** Здесь необходимо взять змачение input и заменить им значение text в staticComment */}
+    let newComment = React.createRef();
+
     const addComment = () => {
-        let staticComment = {id: 1, text: "Hi!", date: "Friday, 1 July 2020"}
-        setComment([...comment, staticComment]);
+        let staticComment = {id: 4, text: newComment.current.value, date: "Friday, 1 July 2020"}
+        setComment([...comments, staticComment]);
     }
 
     return (
         <div className={style.wrapper}>
             <div className={style.inputCommentsArea}>
-                <input className={style.inputText} type="text"/>
+                <input className={style.inputText} ref={newComment}/>
                 <button className={style.sentBtn} onClick={addComment}>sent</button>
             </div>
             <div className={style.outputCommentsArea}>
                 <ul>
-                        {comment.map((item, id) => (<li key={id}><p>{item.text} <br/> {item.date}</p></li>))}
+                        {comments.slice(0).reverse().map((item) => (<li key={item.id}><p>{item.text} <br/> {item.date}</p></li>))}
                 </ul>
             </div>
         </div>
