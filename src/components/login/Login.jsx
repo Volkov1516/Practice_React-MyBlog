fc0bb4a13195c71e413cccef721fe3dbbda400b0
Volkov1516 <from5jforever@gmail.com>
2020-09-28 11:12:25 +0300

    28.09.20

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,11 +2,14 @@ import React, {useState} from  'react'
 import style from './Login.module.css'
 
 const Login = () => {
+    const [inputNameValue, setInputNameValue] = useState('')
     const [inputEmailValue, setInputEmailValue] = useState('')
     const [inputPasswordValue, setInputPasswordValue] = useState('')
 
     return (
         <div className={style.wrapper}>
+            <input className={style.inputText} type="text" placeholder='Name' value={inputNameValue} onChange={e => setInputNameValue(e.target.value)}/>
+            <br/>
             <input className={style.inputText} type="email" placeholder='Email' value={inputEmailValue} onChange={e => setInputEmailValue(e.target.value)}/>
             <br/>
             <input className={style.inputText} type="password" placeholder='Password' value={inputPasswordValue} onChange={e => setInputPasswordValue(e.target.value)}/>
