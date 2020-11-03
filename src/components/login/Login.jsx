868bb1523730926ca5a3a6433419be79ce26ef4a
Volkov1516 <from5jforever@gmail.com>
2020-11-03 20:34:56 +0200

    03.11

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from  'react'
 import style from './Login.module.css'
 
+import {useForm} from 'react-hook-form'
+
 const Login = () => {
     const [inputNameValue, setInputNameValue] = useState('')
     const [inputEmailValue, setInputEmailValue] = useState('')
@@ -8,13 +10,15 @@ const Login = () => {
 
     return (
         <div className={style.wrapper}>
-            <input className={style.inputText} type="text" placeholder='Name' value={inputNameValue} onChange={e => setInputNameValue(e.target.value)}/>
-            <br/>
-            <input className={style.inputText} type="email" placeholder='Email' value={inputEmailValue} onChange={e => setInputEmailValue(e.target.value)}/>
-            <br/>
-            <input className={style.inputText} type="password" placeholder='Password' value={inputPasswordValue} onChange={e => setInputPasswordValue(e.target.value)}/>
-            <br/>
-            <button className={style.loginBtn}>Login</button>
+            <form>
+                <input className={style.inputText} type="text" placeholder='Name' value={inputNameValue} onChange={e => setInputNameValue(e.target.value)}/>
+                <br/>
+                <input className={style.inputText} type="email" placeholder='Email' value={inputEmailValue} onChange={e => setInputEmailValue(e.target.value)}/>
+                <br/>
+                <input className={style.inputText} type="password" placeholder='Password' value={inputPasswordValue} onChange={e => setInputPasswordValue(e.target.value)}/>
+                <br/>
+                <button className={style.loginBtn}>Login</button>
+            </form>
         </div>
     )
 }
