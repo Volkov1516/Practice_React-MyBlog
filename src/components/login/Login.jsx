2822c347760a8d5286ce782287de1d24bf872473
Volkov1516 <from5jforever@gmail.com>
2020-11-06 18:52:05 +0200

    06.11

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -8,16 +8,19 @@ const Login = () => {
     const [inputEmailValue, setInputEmailValue] = useState('')
     const [inputPasswordValue, setInputPasswordValue] = useState('')
 
+    const {register, handleSubmit} = useForm()
+    const onSubmit = data => console.log(data)
+
     return (
         <div className={style.wrapper}>
-            <form>
-                <input className={style.inputText} type="text" placeholder='Name' value={inputNameValue} onChange={e => setInputNameValue(e.target.value)}/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input ref={register} name="name" className={style.inputText} type="text" placeholder='Name' value={inputNameValue} onChange={e => setInputNameValue(e.target.value)}/>
                 <br/>
-                <input className={style.inputText} type="email" placeholder='Email' value={inputEmailValue} onChange={e => setInputEmailValue(e.target.value)}/>
+                <input ref={register} name="email" className={style.inputText} type="email" placeholder='Email' value={inputEmailValue} onChange={e => setInputEmailValue(e.target.value)}/>
                 <br/>
-                <input className={style.inputText} type="password" placeholder='Password' value={inputPasswordValue} onChange={e => setInputPasswordValue(e.target.value)}/>
+                <input ref={register} name="password" className={style.inputText} type="password" placeholder='Password' value={inputPasswordValue} onChange={e => setInputPasswordValue(e.target.value)}/>
                 <br/>
-                <button className={style.loginBtn}>Login</button>
+                <button className={style.loginBtn}>Submit</button>
             </form>
         </div>
     )
